6fd980c46120dca5e6423528f12ed55eac454471
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2023-02-16 09:37:27 +0700

    pertemuan2

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,27 @@
+<?php 
+$name = "Narapati Keysa Anandi";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendahuluan PHP</title>
+</head>
+<body>
+    <h1>
+        <?php echo '"Hello" ' . $name; ?>
+    </h1>
+    <p>
+        <?php echo "Pemrograman Web"; ?>
+    </p>
+    <p>
+        <?php echo '"PENGUMUMAN": Hari ini hari Jum\'at'; ?>
+    </p>
+    <p>
+        <?php echo 10 + 10; ?>
+    </p>
+</body>
+</html>
