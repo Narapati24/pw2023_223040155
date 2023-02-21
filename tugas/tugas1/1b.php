669ea9ad14b6ad7c18997f323757982f7efb8cc0
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2023-02-21 20:05:24 +0700

    memperbaiki tugas 1b

--- tugas/tugas1/1b.php
+++ tugas/tugas1/1b.php
@@ -1,12 +1,11 @@
 <?php
 
-$npm = "55";
-
-echo 'Aku adalah angka <b>' . $npm . '</b> <br>';
-echo 'Jika aku dikali 5, maka aku sekarang menjadi <b>' . $npm * 5 . '</b> <br>';
-echo 'Jika aku dibagi 2, maka aku sekarang menjadi <b>' . $npm / 2 . '</b> <br>';
-echo 'Jika aku ditambah 75, maka aku sekarang menjadi <b>' . $npm + 75 . '</b> <br>';
-echo 'Jika aku dikurang 20, maka aku sekarang menjadi <b>' . $npm - 20 . '</b> <br>';
-
+$npm = 55;
 
 ?>
+
+<p>Aku adalah angka <b><?= $npm; ?></b></p>
+<p>Jika aku dikali 5, maka aku sekarang menjadi <b><?= $npm = $npm * 5; ?></b></p>
+<p>Jika aku dibagi 2, maka aku sekarang menjadi <b><?= $npm = $npm / 2; ?></b></p>
+<p>Jika aku ditambah 75, maka aku sekarang menjadi <b><?= $npm = $npm + 75; ?></b></p>
+<p>Jika aku dikurang 20, maka aku sekarang menjadi <b><?= $npm =  $npm - 20; ?></b></p>
