6f05569cbe494f91b3704e42668f233ebff9bcdf
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2023-02-23 09:35:31 +0700

    pertemuan 3

---
+++ kuliah/pertemuan3/angkot1.php
@@ -0,0 +1,9 @@
+<?php 
+
+$x = 1;
+while ($x <= 10) {
+    echo "Angkot no. $x beroperasi dengan baik <br>";
+    $x++;
+}
+
+?>
---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+// $x = 0;
+// while ($x < 10) {
+//     echo "Hello World <br>";
+//     $x++;
+// }
+
+for ($x = 1; $x <= 50; $x++) {
+    if ($x % 2 == 0 && $x % 3 == 0) {
+        echo "Hello World $x x <br>";
+    }
+}
+
+?>
