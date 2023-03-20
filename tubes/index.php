1cdff550e21f7a2cfc81acdd3ca605ff6da0a67b
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2023-03-21 01:40:08 +0700

    minimum pt1

---
+++ tubes/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pati News Right</title>
+    <!-- reset css -->
+    <link rel="stylesheet" href="css/reset.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+</head>
+<body>
+    <!-- Navbar -->
+<nav class="navbar navbar-expand-lg fixed-top bg-primary navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="" alt="Logo" width="30" height="24">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">News</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Popular</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <button class="btn btn-light" type="submit">Login</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!-- content -->
+<div class="container" style="height: 20px;"></div>
+<div class="container mt-5">
+  <div class="row g-0 bg-body-secondary position-relative">
+    <div class="col-md-6 mb-md-0 p-md-4">
+      <img src="img/sample/sample.png" class="w-100" alt="...">
+    </div>
+    <div class="col-md-6 p-4 ps-md-0">
+      <h5 class="mt-0">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, accusamus!
+      </h5>
+      <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum eum perspiciatis alias fugiat ad doloremque sequi vero assumenda excepturi, tenetur atque est fugit libero nesciunt itaque debitis maiores nemo, similique rerum, vitae harum. Obcaecati exercitationem architecto soluta. Vel ut harum corrupti dolorem beatae, animi cumque error. Earum ut alias libero accusantium vitae voluptatem voluptates distinctio, mollitia dolorem voluptas harum perspiciatis sit et tempore quae animi quisquam, inventore blanditiis adipisci eaque ab sint commodi! Cumque ad necessitatibus perspiciatis, in minus laboriosam ipsa dolore id perferendis explicabo ipsum asperiores, officiis blanditiis molestiae, omnis culpa iste. Odio odit iste eveniet? Exercitationem, alias consectetur.
+      </p>
+      <a href="#" class="stretched-link">Go somewhere</a>
+    </div>
+  </div>
+  
+  <div class="card mt-4" style="width: 18rem;">
+    <img src="img/sample/sample.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card with stretched link</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary stretched-link">Go somewhere</a>
+    </div>
+  </div>
+</div>
+
+<!-- footer -->
+<div class="container-fluid bg-body-secondary ps-5 pt-3 pb-3">
+    ©Narapati Keysa Anandi 2023
+</div>
+
+<!-- Java Script -->
+<script src="js/bootstrap/bootstrap.min.js"></script>
+</body>
+</html>
